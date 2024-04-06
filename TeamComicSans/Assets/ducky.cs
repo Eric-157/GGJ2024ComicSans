7888b2c35b6c513abcbc43848a108625a3290d70
Eric Edwards <59770361+Eric-157@users.noreply.github.com>
2024-04-06 17:37:08 -0400

    Quack Soundeffect
    Quack

--- TeamComicSans/Assets/ducky.cs
+++ TeamComicSans/Assets/ducky.cs
@@ -6,12 +6,19 @@ using UnityEngine.SceneManagement;
 public class ducky : MonoBehaviour
 {
     public Rigidbody2D duck;
+    public AudioClip quack;
+    private AudioSource audioSource;
     // Update is called once per frame
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             duck.MovePosition(duck.position + Vector2.right/20);
+            audioSource.Play();
         }
     }
 }
